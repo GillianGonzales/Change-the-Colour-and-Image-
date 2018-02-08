ccb30efb580c52036d507c341d7e32a18956dc38
GillianGonzales <gillian.gonzales@ocsbstudent.ca>
2018-02-08 14:25:27 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created By Gillian Gonzales
+-- Created On Febuary 08 2018
+--
+-----------------------------------------------------------------------------------------
+
+-- Setting default screen background color to green
+display.setDefault( "background", 0, 1, 0 )
+
+-- Setting a Ninja Boy on the screen
+local image = display.newImageRect( "NinjaBoy.png", 1000, 1000 )
+image.x = 1000
+image.y = 1000
